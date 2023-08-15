19e94878d60ecff9c01ba81faf7c8f3d88ac0a19
Prashant Singh <prashantdineshsingh48@gmail.com>
2023-08-16 03:26:17 +0530

    Checkout/ProductDetail Page Added

--- src/features/cart/Cart.js
+++ src/features/cart/Cart.js
@@ -42,6 +42,7 @@ export default function Cart() {
 
   return (
     <>
+    <div>
       <div className="mx-auto mt-12 bg-white max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
           <h1 className="text-4xl my-5 font-bold tracking-tight text-gray-900">
@@ -111,12 +112,12 @@ export default function Cart() {
             Shipping and taxes calculated at checkout.
           </p>
           <div className="mt-6">
-            <a
-              href="#"
+            <Link
+              to="/checkout"
               className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
             >
               Checkout
-            </a>
+            </Link>
           </div>
           <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
             <p>
@@ -134,6 +135,7 @@ export default function Cart() {
             </p>
           </div>
         </div>
+      </div>
       </div>
     </>
   );
