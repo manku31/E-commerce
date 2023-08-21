84aa938ddc1c92ff2d4095675368561ec2093d0e
Prashant Singh <prashantdineshsingh48@gmail.com>
2023-08-21 19:36:55 +0530

    Pagination Done

--- src/features/product/components/ProductList.js
+++ src/features/product/components/ProductList.js
@@ -4,6 +4,7 @@ import {
   fetchAllProductsAsync,
   fetchProductsByFiltersAsync,
   selectAllProducts,
+  selectTotalItems,
 } from "../productSlice";
 import { Link } from "react-router-dom";
 import { Dialog, Disclosure, Menu, Transition } from "@headlessui/react";
@@ -20,6 +21,7 @@ import {
   PlusIcon,
   Squares2X2Icon,
 } from "@heroicons/react/20/solid";
+import { ITEM_PER_PAGE } from "../../../app/constants";
 
 // for sorting the product list
 const sortOptions = [
@@ -202,48 +204,61 @@ export default function ProductList() {
   const dispatch = useDispatch();
   const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
   const products = useSelector(selectAllProducts);
+  const totalItems = useSelector(selectTotalItems);
+
   const [filter, setFilter] = useState({});
   const [sort, setSort] = useState({});
+  const [page, setPage] = useState(1);
 
   // handle filter
   const handleFilter = (e, section, option) => {
     const newFilter = { ...filter };
     // TODO : on server it will suppport multiple catagers
     if (e.target.checked) {
-
-      
-     
-      if(newFilter[section.id]) {
-
+      // if any filter option is checked box is click
+      if (newFilter[section.id]) {
+        // array is alreday created with atleast one value init
         newFilter[section.id].push(option.value); // adding section : value in new Filter in form of array
-      }else {
+      } else {
+        // for 1st filter checkbox is click
         newFilter[section.id] = [option.value];
-
       }
-
     } else {
-
-      const index = newFilter[section.id].findIndex((fi) => fi === option.value);
-      newFilter[section.id].splice(index, 1);   // removing section : value in new Filter from array
-
+      // if any filter option is unchecked box is click
+      const index = newFilter[section.id].findIndex(
+        (fi) => fi === option.value
+      );
+      newFilter[section.id].splice(index, 1); // removing section : value in new Filter from array
     }
 
-    console.log({newFilter});
+    // console.log({ newFilter });
 
     setFilter(newFilter);
   };
 
   // handle sorting
   const handleSort = (e, option) => {
-    const sort = {_sort: option.sort, _order: option.order };
-    console.log({sort});
+    const sort = { _sort: option.sort, _order: option.order };
+    // console.log( sort );
     setSort(sort);
   };
 
-  useEffect(() => {
-    dispatch(fetchProductsByFiltersAsync({filter, sort}));
-  }, [dispatch, filter, sort]);
+  // handle page
+  const handlePage = (page) => {
+    console.log({ page });
+    setPage(page);
+  };
 
+  useEffect(() => {
+    const pagination = { _page: page, _limit: ITEM_PER_PAGE };
+    dispatch(fetchProductsByFiltersAsync({ filter, sort, pagination }));
+  }, [dispatch, filter, sort, page]);
+
+  useEffect( () => {
+    setPage(1);
+  }, [totalItems, sort])
+
+  // main branch of ProductList
   return (
     <div className="bg-white">
       <div>
@@ -341,7 +356,12 @@ export default function ProductList() {
           </section>
 
           {/* Pagination */}
-          <Pagination></Pagination>
+          <Pagination
+            page={page}
+            setPage={setPage}
+            handlePage={handlePage}
+            totalItems={totalItems}
+          ></Pagination>
         </main>
       </div>
     </div>
@@ -519,7 +539,7 @@ function DesktopFilter({ handleFilter }) {
   );
 }
 
-function Pagination() {
+function Pagination({ page, setPage, handlePage, totalItems }) {
   return (
     <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
       <div className="flex flex-1 justify-between sm:hidden">
@@ -539,9 +559,19 @@ function Pagination() {
       <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
         <div>
           <p className="text-sm text-gray-700">
-            Showing <span className="font-medium">1</span> to{" "}
-            <span className="font-medium">10</span> of{" "}
-            <span className="font-medium">97</span> results
+            Showing{" "}
+            <span className="font-medium">
+              {(page - 1) * ITEM_PER_PAGE + 1}
+            </span>{" "}
+            to{" "}
+            <span className="font-medium">
+              {/* ITEM_PER_PAGE is constant value */}
+              {page * ITEM_PER_PAGE > totalItems
+                ? totalItems
+                : page * ITEM_PER_PAGE}
+            </span>{" "}
+            of
+            <span className="font-medium">{totalItems}</span> results
           </p>
         </div>
         <div>
@@ -557,19 +587,23 @@ function Pagination() {
               <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
             </a>
             {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
-            <a
-              href="#"
-              aria-current="page"
-              className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              1
-            </a>
-            <a
-              href="#"
-              className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-            >
-              2
-            </a>
+
+            {Array.from({ length: Math.ceil(totalItems / ITEM_PER_PAGE) }).map(
+              (elment, index) => (
+                <div
+                  key={index + 1}
+                  onClick={(e) => handlePage(index + 1)}
+                  aria-current="page"
+                  className={`relative cursor-pointer z-10 inline-flex items-center ${
+                    index + 1 === page
+                      ? " bg-indigo-600 text-white"
+                      : "text-gray-400"
+                  }   px-4 py-2 text-sm font-semibold focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
+                >
+                  {index + 1} {/* index start from 0 and page start form 1 */}
+                </div>
+              )
+            )}
 
             <a
               href="#"
--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -8,20 +8,20 @@ export function fetchAllProducts() {
   });
 }
 
+export function fetchProductsByFilters(filter, sort, pagination) {
+  // filter = {"category":["smartphone", "laptop", "mobile"]} => ?category=smartphone&categeory=laptop -> url ? category = smartphone & categeory = laptop
+  // sort = {_sort: "price", _order: "dec"} => ?_sort=price&_order=asc -> url ? _sort = price & _order = asc
+  // pagination = {_page : 1, limit = 10} => ?_page=1&_limit=10 -> url ? _page = 7 & _limit = 20
 
-
-export function fetchProductsByFilters(filter, sort) {
-  // filter = {"category":["smartphone", "laptop", "mobile"]}
   // TODO : on server we will support multi values
-  // console.log(filter);
   let queryString = "";
+
   for (let key in filter) {
     const categoryValues = filter[key];
 
-    if(categoryValues.length > 0) {
-      // console.log(categoryValues);
+    if (categoryValues.length > 0) {
       const lastCategoryValues = categoryValues[categoryValues.length - 1];
-      // console.log(lastCategoryValues);
+
       queryString += `${key}=${lastCategoryValues}&`;
     }
   }
@@ -30,12 +30,18 @@ export function fetchProductsByFilters(filter, sort) {
     queryString += `${key}=${sort[key]}&`;
   }
 
+  for (let key in pagination) {
+    queryString += `${key}=${pagination[key]}&`;
+  }
+
+
   return new Promise(async (resolve) => {
     //TODO: we will not hard-code server URL here
     const response = await fetch(
       "http://localhost:8080/products?" + queryString
     );
     const data = await response.json();
-    resolve({ data });
+    const totalItems = await response.headers.get("X-Total-Count");
+    resolve({ data : {products : data, totalItems : +totalItems }});
   });
 }
